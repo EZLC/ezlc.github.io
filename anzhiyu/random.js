a4c5f5563978d19f133c848615ca94bc0c2bc1d9
ezlc <9969486@qq.com>
2023-08-22 21:26:21 +0800

    Site updated: 2023-08-22 21:26:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2017/05/26/Java常用类的概述和使用/","2017/05/26/String类的概述和使用/","2017/05/26/正则表达式的概述和使用/","2017/05/28/可变字符串类和日期相关类/","2017/05/29/集合类库/","2021/12/10/Log4j远程代码执行漏洞复现/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2017/05/26/Java常用类的概述和使用/","2017/05/26/String类的概述和使用/","2017/05/26/正则表达式的概述和使用/","2017/05/28/可变字符串类和日期相关类/","2017/05/29/集合类库/","2021/12/10/Log4j远程代码执行漏洞复现/","2020/04/10/JDK1.6-1.8方法区的变化/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
